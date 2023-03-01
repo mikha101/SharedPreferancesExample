8c169f6ea0dcbacf3f9ff663f9748a96c51e4abb
«Elena <helenvit599@gmail.com>
2023-03-01 21:07:19 +0500

    Finished application

--- app/src/main/java/ru/synergy/sharedpreferancesexample/MainActivity.java
+++ app/src/main/java/ru/synergy/sharedpreferancesexample/MainActivity.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     EditText et;
     Button btnSave, btnLoad;
     SharedPreferences sharedPreferences;
     final String SAVED_TEXT = "saved text";
+    private Log Lod;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +29,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnSave = (Button) findViewById(R.id.btnSave);
         btnLoad = (Button) findViewById(R.id.btnLoad);
 
-        btnSave.setOnClickListener(this);
-        btnLoad.setOnClickListener(this);
+//        btnSave.setOnClickListener(this);
+//        btnLoad.setOnClickListener(this);
+
+        loadText();
+        et.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                saveText();
+                Lod.d(SAVED_TEXT,et.getText().toString());
+                return true;
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btnSave:
-                saveText();
-                break;
-            case R.id.btnLoad:
-                loadText();
-                break;
-            default:
-                break;
-        }
-    }
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()){
+//            case R.id.btnSave:
+//                saveText();
+//                break;
+//            case R.id.btnLoad:
+//                loadText();
+//                break;
+//            default:
+//                break;
+//        }
+//    }
 
     private void loadText() {
         sharedPreferences = getPreferences(MODE_PRIVATE);
@@ -57,5 +71,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ed.commit();
         Toast.makeText(this, "Text saved", Toast.LENGTH_LONG).show();
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveText();
     }
 }
