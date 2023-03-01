f72004b7d613ec414d2d5ee30feaeac6de58f081
«Elena <helenvit599@gmail.com>
2023-03-01 20:49:50 +0500

    Working sharedPreferences

--- app/src/main/java/ru/synergy/sharedpreferancesexample/MainActivity.java
+++ app/src/main/java/ru/synergy/sharedpreferancesexample/MainActivity.java
@@ -2,13 +2,60 @@ package ru.synergy.sharedpreferancesexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText et;
+    Button btnSave, btnLoad;
+    SharedPreferences sharedPreferences;
+    final String SAVED_TEXT = "saved text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = (EditText) findViewById(R.id.etText);
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnLoad = (Button) findViewById(R.id.btnLoad);
+
+        btnSave.setOnClickListener(this);
+        btnLoad.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnSave:
+                saveText();
+                break;
+            case R.id.btnLoad:
+                loadText();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void loadText() {
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        String saveText = sharedPreferences.getString(SAVED_TEXT, "Ничего не сохранено в Shared Preferences");
+        et.setText((saveText));
+        Toast.makeText(this, "Text Loaded", Toast.LENGTH_LONG).show();
+    }
+
+    private void saveText() {
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = sharedPreferences.edit();
+        ed.putString(SAVED_TEXT, et.getText().toString());
+        ed.commit();
+        Toast.makeText(this, "Text saved", Toast.LENGTH_LONG).show();
+
     }
 }
